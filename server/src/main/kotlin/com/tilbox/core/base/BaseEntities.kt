a4a1f62721358a152abe6d564941e6bd38a34b0a
K.S.KIM <nullable@kakao.com>
2021-10-31 19:45:44 +0900

    feat: base 엔티티 추가

---
+++ server/src/main/kotlin/com/tilbox/core/base/BaseEntities.kt
@@ -0,0 +1,52 @@
+package com.tilbox.core.base
+
+
+import org.springframework.data.domain.AbstractAggregateRoot
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BaseEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
+
+@MappedSuperclass
+abstract class BaseRootEntity<T : AbstractAggregateRoot<T>>(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) : AbstractAggregateRoot<T>() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BaseEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
