0600cc81c0dcdda3c884ce6f646bc8436308b790
K.S.KIM <nullable@kakao.com>
2021-10-31 22:44:27 +0900

    feat: JPA Auditing 설정 추가

---
+++ server/src/main/kotlin/com/tilbox/core/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.tilbox.core.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig {}
