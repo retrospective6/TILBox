95314483c57243c9179c00a2b8df50343c5af586
K.S.KIM <nullable@kakao.com>
2021-11-07 19:50:13 +0900

    test: 통합 테스트 데이터베이스 초기화 서비스 추가

---
+++ server/src/test/kotlin/com/tilbox/base/test/CaseConvertUtils.kt
@@ -0,0 +1,13 @@
+package com.tilbox.base.test
+
+class CaseConvertUtils {
+    companion object {
+        val CAMEL_REGEX = "(?<=[a-zA-Z])[A-Z]".toRegex()
+
+        fun camelToSnakeCase(value: String): String {
+            return CAMEL_REGEX.replace(value) {
+                "_${value}"
+            }.lowercase()
+        }
+    }
+}
---
+++ server/src/test/kotlin/com/tilbox/base/test/RestControllerTest.kt
@@ -0,0 +1,35 @@
+package com.tilbox.base.test
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+abstract class RestControllerTest {
+
+    @field:Autowired
+    private lateinit var databaseCleanUp: DatabaseCleanUp
+
+    lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    internal fun setUp(
+        webApplicationContext: WebApplicationContext,
+    ) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .build()
+    }
+
+    @AfterEach
+    fun cleanupAfterEach() {
+        databaseCleanUp.truncate()
+    }
+}
