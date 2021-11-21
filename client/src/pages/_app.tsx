c8bda2e9695717413164804f230b0e7bd769e0db
hwookim <gusdn9754@gmail.com>
2021-11-21 13:50:11 +0900

    fix: 기본 생성되는 클래스/함수 이름 변경

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function TILApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
+export default TILApp;
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   DocumentContext,
 } from 'next/document';
 
-class MyDocument extends Document {
+class TILDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
@@ -31,4 +31,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
+export default TILDocument;
