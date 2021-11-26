52d2d126e3439c182b43c78d98f8b611641b048f
K.S.KIM <nullable@kakao.com>
2021-11-26 15:55:39 +0900

    feat: jwt 인증 필터 추가

---
+++ server/src/main/kotlin/com/tilbox/api/security/JwtAuthenticationFilter.kt
@@ -0,0 +1,32 @@
+package com.tilbox.api.security
+
+import io.jsonwebtoken.JwtException
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Configuration
+class JwtAuthenticationFilter(private val jwtTokenProvider: JwtTokenProvider) : OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        authenticate(request)
+        filterChain.doFilter(request, response)
+    }
+
+    private fun authenticate(request: HttpServletRequest) {
+        try {
+            val token = request.getHeader(HttpHeaders.AUTHORIZATION)
+            val payload = jwtTokenProvider.extractPayload(token)
+            SecurityContextHolder.getContext().authentication = UsernamePasswordAuthenticationToken(payload.userId, "", listOf(SimpleGrantedAuthority(payload.userRole.title)))
+        } catch (exception: JwtException) {
+            logger.info("로그인에 실패했습니다. message=${exception.message}")
+        } catch (exception: IllegalArgumentException) {
+            logger.info("올바르지 않은 값을 포함하고 있습니다. message=${exception.message}")
+        }
+    }
+}
