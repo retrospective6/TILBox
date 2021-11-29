626a9f6709ea0d4f319384f791b600a3fb1e172c
K.S.KIM <nullable@kakao.com>
2021-11-29 22:52:12 +0900

    refactor: 사용자 회원가입 dto 검증 어노테이션 추가

--- server/src/main/kotlin/com/tilbox/api/user/ui/UserRestController.kt
+++ server/src/main/kotlin/com/tilbox/api/user/ui/UserRestController.kt
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v1/users")
 class UserRestController(private val emailUserCreateService: EmailUserCreateService) {
     @PostMapping
-    fun createUser(@RequestBody request: UserCreateRequest): ResponseEntity<UserCreateResponse> {
+    fun createUser(@Valid @RequestBody request: UserCreateRequest): ResponseEntity<UserCreateResponse> {
         val response = emailUserCreateService.createUser(request)
         return ResponseEntity
             .created(URI.create("/v1/users/${response.myTilAddress}"))
