d37906998a58e0475ffdeb982b3ea038f8580213
K.S.KIM <nullable@kakao.com>
2021-11-29 22:46:35 +0900

    feat: 이메일 기반 사용자 제공 서비스 추가

---
+++ server/src/main/kotlin/com/tilbox/api/user/application/EmailPasswordUserDetailsService.kt
@@ -0,0 +1,17 @@
+package com.tilbox.api.user.application
+
+import com.tilbox.api.security.UserPrincipal
+import com.tilbox.core.user.domain.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class EmailPasswordUserDetailsService(private val userRepository: UserRepository) : UserDetailsService {
+    override fun loadUserByUsername(email: String): UserDetails {
+        val user = userRepository.findByEmail(email)
+            ?: throw UsernameNotFoundException("사용자를 찾을 수 없습니다. email=${email}")
+        return UserPrincipal(user.id, user.email, user.password, user.userRole)
+    }
+}
