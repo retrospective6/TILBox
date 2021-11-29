0a0e2eed90a92c25713cfa10744059e97724fd5d
K.S.KIM <nullable@kakao.com>
2021-11-29 22:54:47 +0900

    refactor: 시큐리티 필터 추가 및 세션 관리전략 수정

--- server/src/main/kotlin/com/tilbox/api/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/tilbox/api/config/SecurityConfig.kt
@@ -1,17 +1,23 @@
 package com.tilbox.api.config
 
+import com.tilbox.api.security.JwtAuthenticationFilter
+import com.tilbox.api.security.JwtCreationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.logout.LogoutFilter
+import org.springframework.security.web.header.HeaderWriterFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(private val jwtCreationFilter: JwtCreationFilter, private val customUsernamePasswordAuthenticationFilter: UsernamePasswordAuthenticationFilter, private val jwtAuthenticationFilter: JwtAuthenticationFilter) : WebSecurityConfigurerAdapter() {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder()
@@ -19,6 +25,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http {
+            formLogin {
+            }
             headers {
                 frameOptions { sameOrigin }
             }
@@ -27,6 +35,10 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             authorizeRequests {
                 authorize("/**", permitAll)
             }
+            addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            addFilterAfter(customUsernamePasswordAuthenticationFilter, LogoutFilter::class.java)
+            addFilterAfter(jwtCreationFilter, HeaderWriterFilter::class.java)
+            sessionManagement { SessionCreationPolicy.STATELESS }
         }
     }
 }
