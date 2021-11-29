89c13812a36b3ba699b3d627eade6762b0bd9c81
K.S.KIM <nullable@kakao.com>
2021-11-29 22:45:40 +0900

    refactor: jwt 토큰 제공자가 UserPrincipal을 받도록 수정

--- server/src/main/kotlin/com/tilbox/api/security/JwtProvider.kt
+++ server/src/main/kotlin/com/tilbox/api/security/JwtProvider.kt
@@ -1,41 +1,41 @@
 package com.tilbox.api.security
 
-import com.tilbox.core.user.domain.value.UserRole
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
 import java.time.Duration
 import java.util.*
 import javax.crypto.SecretKey
 
 @Component
-class JwtTokenProvider {
+class JwtProvider {
     companion object {
         private val SECRET_KEY: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256) // TODO secret key 변경
         private val EXPIRATION_TIME_MILLISECOND: Long = Duration.ofDays(30L).toMillis()
         private const val USER_ID_CLAIM_KEY = "userId";
-        private const val AUTHORITY_CLAIM_KEY = "role";
+        private const val ROLE_CLAIM_KEY = "role";
     }
 
-    fun createToken(payload: JwtPayload): String {
+    fun createToken(userPrincipal: UserPrincipal): String {
         val now = Date()
         val expiration = Date(now.time + EXPIRATION_TIME_MILLISECOND)
 
         return Jwts.builder()
-            .setClaims(mutableMapOf(USER_ID_CLAIM_KEY to payload.userId, AUTHORITY_CLAIM_KEY to payload.userRole.title))
+            .setClaims(mutableMapOf(USER_ID_CLAIM_KEY to userPrincipal.userId, ROLE_CLAIM_KEY to userPrincipal.authorities.first().toString()))
             .setIssuedAt(now)
             .setExpiration(expiration)
             .signWith(SECRET_KEY)
             .compact()
     }
 
-    fun extractPayload(token: String): JwtPayload {
+    fun extractPayload(token: String): UserPrincipal {
         val claims = getClaims(token)
-        val userId = claims[USER_ID_CLAIM_KEY].toString().toLong()
-        val role = UserRole.from(claims[AUTHORITY_CLAIM_KEY].toString())
-        return JwtPayload(userId, role)
+        val userId = claims.get(USER_ID_CLAIM_KEY, Long::class.java)
+        val role = claims.get(ROLE_CLAIM_KEY, String::class.java)
+        return UserPrincipal(userId, null, null, listOf(SimpleGrantedAuthority(role)))
     }
 
     private fun getClaims(token: String): Claims {
