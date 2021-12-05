bd0250fca44c45ff93be238ebf3105b94b26c31b
ksy90101 <ksy90101@gmail.com>
2021-12-05 19:46:51 +0900

    refactor: index.styles.tsx 삭제

--- client/src/pages/signUp/index.tsx
+++ client/src/pages/signUp/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ImgSelector from '@/components/ImgSelector';
 import Form from '@/components/signUp/form/Form';
-import * as Styled from '@/pages/signUp/index.styles';
+import styled from '@emotion/styled';
 
 export default function Index(): JSX.Element {
   const onImgSubmit = () => {
@@ -9,13 +9,37 @@ export default function Index(): JSX.Element {
   };
 
   return (
-    <Styled.Container>
-      <Styled.Title>회원가입</Styled.Title>
-      <Styled.ProfileWrap>
+    <Container>
+      <Title>회원가입</Title>
+      <ProfileWrap>
         <ImgSelector onSubmit={onImgSubmit} />
         <p>프로필 사진은 수정 가능합니다.</p>
-      </Styled.ProfileWrap>
+      </ProfileWrap>
       <Form />
-    </Styled.Container>
+    </Container>
   );
 }
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ProfileWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0 24px;
+
+  p {
+    font-size: 8px;
+    color: #888888;
+    margin-top: 8px;
+  }
+`;
