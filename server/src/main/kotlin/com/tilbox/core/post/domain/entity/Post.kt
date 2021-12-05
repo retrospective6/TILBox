22d22600604f198989ebd1426f87fec1fac39142
mintjordy <stj930@gmail.com>
2021-12-05 20:16:25 +0900

    refactor: post 썸네일 URL 필드 타입 nullable로 변경

--- server/src/main/kotlin/com/tilbox/core/post/domain/entity/Post.kt
+++ server/src/main/kotlin/com/tilbox/core/post/domain/entity/Post.kt
@@ -1,6 +1,8 @@
 package com.tilbox.core.post.domain.entity
 
 import com.tilbox.core.base.BaseRootEntity
+import com.tilbox.core.post.domain.value.PostVisibleLevel
+import com.tilbox.core.post.domain.value.Tags
 import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Embedded
@@ -15,33 +17,32 @@ class Post(
     private val userId: Long,
 
     @Column(name = "title", nullable = false, length = 40, columnDefinition = "TIL 제목")
-    private val title: String,
+    private var title: String,
 
     @Lob
-    private val content: String,
+    private var content: String,
 
     @Column(name = "summary", nullable = false, length = 150, columnDefinition = "게시물 요약")
-    private val summary: String,
+    private var summary: String,
 
     @Embedded
     private val tags: Tags,
 
     @Lob
-    private val thumbnail: String,
+    private var thumbnailUrl: String?,
 
     @Column(name = "visibleLevel", nullable = false, length = 10, columnDefinition = "게시물 공개범위")
     @Enumerated(EnumType.STRING)
-    private val visibleLevel: PostVisibleLevel,
-
-    id: Long = 0L
-) : BaseRootEntity<Post>(id) {
+    private var visibleLevel: PostVisibleLevel,
 
     @Column(name = "likeCount", nullable = false, columnDefinition = "댓글 좋아요 갯수")
-    private val likeCount: Long = 0
+    private var likeCount: Long = 0,
 
     @Column(name = "created_at", nullable = false, columnDefinition = "게시물 작성시간")
-    lateinit var createdAt: LocalDateTime
+    private val createdAt: LocalDateTime,
 
     @Column(name = "modified_at", nullable = false, columnDefinition = "게시물 수정시간")
-    lateinit var updatedAt: LocalDateTime
-}
+    private var updatedAt: LocalDateTime,
+
+    id: Long = 0L
+) : BaseRootEntity<Post>(id)
