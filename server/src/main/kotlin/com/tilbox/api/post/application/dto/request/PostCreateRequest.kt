f44e470fb19a0be471344b8f31f34e9da83019e3
mintjordy <stj930@gmail.com>
2021-12-05 20:24:40 +0900

    feat: 게시글 생성요청 dto 및 repository 추가

---
+++ server/src/main/kotlin/com/tilbox/api/post/application/dto/request/PostCreateRequest.kt
@@ -0,0 +1,34 @@
+package com.tilbox.api.post.application.dto.request
+
+import com.tilbox.core.post.domain.value.PostVisibleLevel
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+import javax.validation.constraints.NotBlank
+
+@ApiModel("작성 게시글 정보", description = "게시글 작성시 필요한 정보")
+data class PostCreateRequest(
+    @ApiModelProperty("TIL 작성 회원 ID번호", required = true, example = "30")
+    @field:NotBlank
+    val userId: Long,
+
+    @ApiModelProperty("TIL 제목", required = true, example = "12/4일 TIL")
+    @field:NotBlank
+    val title: String,
+
+    @ApiModelProperty("TIL 본문", required = true, example = "java8 스트림 API 목록: 1. filter(), 2. map, 3. collect")
+    @field:NotBlank
+    val content: String,
+
+    @ApiModelProperty("TIL Summary", required = true, example = "java8 스트림 API 목록을 정리해보았습니다.")
+    val summary: String,
+
+    @ApiModelProperty("TIL 태그목록", required = false, example = "['자바', 'Stream']")
+    val tags: List<String>,
+
+    @ApiModelProperty("TIL 썸네일 URL", required = true, example = "https://s3.amazonaws.com/s3/bucket/thumbnail.jpg")
+    val thumbnailUrl: String? = null,
+
+    @ApiModelProperty("TIL 공개범위 설정", required = true, example = "PUBLIC")
+    @field:NotBlank
+    val visibleLevel: PostVisibleLevel
+)
