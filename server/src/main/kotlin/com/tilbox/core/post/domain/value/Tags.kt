e7c9fb9517c44c2911ea4fa424eaf187003db33c
mintjordy <stj930@gmail.com>
2021-12-05 20:18:36 +0900

    feat: 태그 목록 정적 생성자 추가

--- server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
+++ server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
@@ -10,4 +10,12 @@ class Tags(
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "tags")
     private val items: MutableList<Tag> = mutableListOf()
-)
+) {
+    companion object {
+        fun of(items: List<String>): Tags {
+            val tags = items.map { Tag(it) }
+                .toMutableList()
+            return Tags(tags)
+        }
+    }
+}
