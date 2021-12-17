06b1d7c8fed22d4c7f746c1591f9581c76fc3453
hwookim <gusdn9754@gmail.com>
2021-12-17 20:47:29 +0900

    refactor: MockData 분리

---
+++ client/__mocks__/MockData.ts
@@ -0,0 +1,31 @@
+import Post from '@/types/Post';
+
+export const POST: Post = {
+  title: '여덟글자하이용',
+  user: {
+    nickname: 'KS-KIM',
+    image: 'https://avatars.githubusercontent.com/u/20358042?s=48&v=4',
+  },
+  thumbnail: '#D45438 #FEA768',
+  description:
+    '요약글 미입력시 본문 앞 내용을 불러와서 여기에 3줄까지 표시됩니다. 이후는 ‘...’ 말 줄임표를 통해 나타납니다.(요약글 동일)',
+  likes: 0,
+  comments: 0,
+  createdAt: '21.12.15',
+  tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag', 'tag', 'tag', 'tag'],
+};
+
+export const ADMIN_POST: Post = {
+  title: '뭔가 공지를 하는 글입니다.',
+  user: {
+    nickname: '운영자',
+    image: 'https://avatars.githubusercontent.com/u/20358042?s=48&v=4',
+  },
+  thumbnail: '#000000 #000000',
+  description:
+    '요약글 미입력시 본문 앞 내용을 불러와서 여기에 3줄까지 표시됩니다. 이후는 ‘...’ 말 줄임표를 통해 나타납니다.(요약글 동일)',
+  likes: 1,
+  comments: 10,
+  createdAt: '21.12.15',
+  tags: ['tag', 'tag'],
+};
