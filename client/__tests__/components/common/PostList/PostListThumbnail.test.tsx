50c5ee10af6343600360528a86fe40723175480b
hwookim <gusdn9754@gmail.com>
2021-12-19 17:54:22 +0900

    fix: css를 통해 처리되는 부분 test 삭제

--- client/__tests__/components/common/PostList/PostListThumbnail.test.tsx
+++ client/__tests__/components/common/PostList/PostListThumbnail.test.tsx
@@ -38,24 +38,3 @@ describe('without thumbnail img', () => {
     expect(() => getByRole('img')).toThrowError();
   });
 });
-
-describe('with title length <= 20', () => {
-  test.each([
-    '여덟글자이하에용',
-    '이건열두글자문장입니다요',
-    '이건스무글자짜리문장입니다안녕하십니까요',
-  ])('render title ', (title) => {
-    const { getByText } = renderThumbnail({ title });
-    getByText(title);
-  });
-});
-
-describe('with title length > 20', () => {
-  test.each([
-    '이건스물한글자짜리문장이에요겁나게길구만유',
-    '이건조금더길게적은문장입니다전체길이는스물일곱글자네요',
-  ])('render title only 20 length and add ... ', (title) => {
-    const { getByText } = renderThumbnail({ title });
-    getByText(title.substring(0, 20) + '...');
-  });
-});
