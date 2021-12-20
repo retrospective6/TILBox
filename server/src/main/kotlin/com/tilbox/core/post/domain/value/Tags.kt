cc6207d342f510e7635786a40f1dc285dbd77452
mintjordy <stj930@gmail.com>
2021-12-20 14:45:10 +0900

    feat: tag 테이블 index 추가

--- server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
+++ server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
@@ -4,11 +4,26 @@ import javax.persistence.CollectionTable
 import javax.persistence.ElementCollection
 import javax.persistence.Embeddable
 import javax.persistence.FetchType
+import javax.persistence.Index
+import javax.persistence.JoinColumn
+import javax.persistence.UniqueConstraint
 
 @Embeddable
 class Tags(
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "tags")
+    @CollectionTable(
+        name = "tag",
+        joinColumns = [
+            JoinColumn(name = "post_id")
+        ],
+        uniqueConstraints = [
+            UniqueConstraint(name = "UK_post_id_tag_name", columnNames = ["post_id", "name"])
+        ],
+        indexes = [
+            Index(name = "IDX_post_id", columnList = "post_id"),
+            Index(name = "IDX_tag_name", columnList = "name")
+        ]
+    )
     private val items: MutableList<Tag> = mutableListOf()
 ) {
     companion object {
