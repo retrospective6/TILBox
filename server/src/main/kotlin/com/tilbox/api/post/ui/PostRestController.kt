38277025f4cbd42ab883fbda65fec9a1eda37eb4
mintjordy <stj930@gmail.com>
2021-12-20 17:16:46 +0900

    feat: 게시글 수정 API 추가

--- server/src/main/kotlin/com/tilbox/api/post/ui/PostRestController.kt
+++ server/src/main/kotlin/com/tilbox/api/post/ui/PostRestController.kt
@@ -2,12 +2,15 @@ package com.tilbox.api.post.ui
 
 import com.tilbox.api.post.application.PostService
 import com.tilbox.api.post.application.dto.request.PostCreateRequest
+import com.tilbox.api.post.application.dto.request.PostUpdateRequest
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -30,5 +33,15 @@ class PostRestController(private val postService: PostService) {
         return ResponseEntity
             .created(URI.create("/v1/posts/$createdPostId"))
             .body(createdPostId)
+    }
+
+    @ApiResponses(
+        ApiResponse(code = 200, message = "게시글 수정 완료"),
+        ApiResponse(code = 400, message = "필수 값 누락으로 수정 실패")
+    )
+    @PutMapping("/{postId}")
+    fun updatePost(@PathVariable postId: Long, @RequestBody request: PostUpdateRequest): ResponseEntity<Long> {
+        val updatedPostId = postService.updatePost(postId, request, LocalDateTime.now())
+        return ResponseEntity.ok(updatedPostId)
     }
 }
--- server/src/test/kotlin/com/tilbox/api/post/ui/PostRestControllerTest.kt
+++ server/src/test/kotlin/com/tilbox/api/post/ui/PostRestControllerTest.kt
@@ -2,13 +2,24 @@ package com.tilbox.api.post.ui
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tilbox.api.post.application.dto.request.PostCreateRequest
+import com.tilbox.api.post.application.dto.request.PostUpdateRequest
 import com.tilbox.base.test.RestControllerTest
+import com.tilbox.core.post.domain.entity.Post
+import com.tilbox.core.post.domain.repository.PostRepository
 import com.tilbox.core.post.domain.value.PostVisibleLevel
+import com.tilbox.core.post.domain.value.Tags
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
+import java.time.LocalDateTime
 
 class PostRestControllerTest() : RestControllerTest() {
+
+    @field:Autowired
+    private lateinit var postRepository: PostRepository
+
     @Test
     fun `게시글 작성 요청이 오면 게시글 생성후, 생성된 게시글의 ID를 반환한다`() {
         val request = PostCreateRequest(
@@ -36,6 +47,42 @@ class PostRestControllerTest() : RestControllerTest() {
             contentType = MediaType.APPLICATION_JSON
         }.andExpect {
             status { isCreated() }
+        }
+    }
+
+    @Test
+    fun `게시글 수정 요청이 오면 게시글 수정후, 수정된 게시글의 ID를 반환한다`() {
+        val savedPost = postRepository.save(
+            Post(
+                userId = 1L,
+                title = "title",
+                content = "content",
+                summary = "summary",
+                tags = Tags.of(listOf("tag1")),
+                thumbnailUrl = "thumbnailUrl",
+                visibleLevel = PostVisibleLevel.PRIVATE,
+                createdAt = LocalDateTime.of(2021, 12, 10, 10, 35),
+                updatedAt = LocalDateTime.of(2021, 12, 10, 10, 35)
+            )
+        )
+
+        val postId = savedPost.id
+        val request = PostUpdateRequest(
+            userId = 3L,
+            title = "10/23일 TIL",
+            content = "new content",
+            summary = "new summary",
+            tags = listOf("newTag1", "newTag2"),
+            thumbnailUrl = "https://s3.amazonaws/sample/bucket/file.jpg",
+            visibleLevel = PostVisibleLevel.PUBLIC
+        )
+
+        mockMvc.put("/v1/posts/$postId") {
+            content = jacksonObjectMapper().writeValueAsBytes(request)
+            accept = MediaType.APPLICATION_JSON
+            contentType = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
         }
     }
 }
