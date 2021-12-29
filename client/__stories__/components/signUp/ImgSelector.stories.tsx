3005416d0b4f5721cdf636ba6358f7ffc66f9650
ksy90101 <ksy90101@gmail.com>
2021-12-29 14:26:20 +0900

    refactor: 폴더 명 변경

--- client/__stories__/components/signUp/ImgSelector.stories.tsx
+++ client/__stories__/components/signUp/ImgSelector.stories.tsx
@@ -4,7 +4,7 @@ import ImgSelector from '@/components/signUp/ImgSelector';
 
 export default {
   component: ImgSelector,
-  title: 'signup/ImgSelector',
+  title: 'signUp/ImgSelector',
   parameters: {
     actions: { argTypesRegex: '^on.*' },
   },
