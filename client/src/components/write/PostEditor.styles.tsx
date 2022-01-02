c10cc9735ca8d243def65c9aa31665b2c4f620ac
hwookim <gusdn9754@gmail.com>
2022-01-02 16:54:24 +0900

    feat: PostEditor 구현

---
+++ client/src/components/write/PostEditor.styles.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import FONT from '@/styles/font';
+
+export const Input = styled.input`
+  width: 100%;
+  margin-bottom: 30px;
+  padding-bottom: 8px;
+  border: none;
+  border-bottom: 2px solid #f3f3f3;
+  outline: none;
+  ${FONT.title1};
+`;
--- client/src/components/write/PostEditor.tsx
+++ client/src/components/write/PostEditor.tsx
@@ -1,32 +1,57 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, ChangeEvent } from 'react';
+import * as Styled from './PostEditor.styles';
+
 import { Editor } from '@toast-ui/react-editor';
 import EditorWithForwardedRef from '@/components/write/EditorWithForwardedRef';
 
 export interface PostEditorProps {
-  onSubmit: () => void;
+  title?: string;
+  content?: string;
+  onChange: (title: string, content: string) => void;
 }
 
 export default function PostEditor(props: PostEditorProps): JSX.Element {
-  const [value, setValue] = useState<string>('');
+  const { onChange } = props;
+
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
   const editorRef = useRef<Editor>(null);
 
-  const handleChange = () => {
+  const handleChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setTitle(value);
+  };
+
+  const handleChangeContent = () => {
     if (!editorRef.current) {
       return;
     }
 
     const editor = editorRef.current.getInstance();
-    setValue(editor.getMarkdown());
+    setContent(editor.getMarkdown());
+  };
+
+  const handleBlur = () => {
+    onChange(title, content);
   };
 
   return (
-    <EditorWithForwardedRef
-      height="80vh"
-      initialValue={value}
-      initialEditType="markdown"
-      previewStyle="vertical"
-      onChange={handleChange}
-      ref={editorRef}
-    />
+    <>
+      <Styled.Input
+        data-testid="title-input"
+        value={title}
+        onChange={handleChangeTitle}
+        onBlur={handleBlur}
+      />
+      <EditorWithForwardedRef
+        height="80vh"
+        initialValue={content}
+        initialEditType="markdown"
+        previewStyle="vertical"
+        onChange={handleChangeContent}
+        onBlur={handleBlur}
+        ref={editorRef}
+      />
+    </>
   );
 }
