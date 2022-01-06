fefe00206451cefb7da575ef7b0528d9733882a6
hwookim <gusdn9754@gmail.com>
2022-01-07 02:49:30 +0900

    fix: modal 출력 시 body witdh가 변하는 문제 해결

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+import styled from '@emotion/styled';
 
 export default function TILApp({
   Component,
   pageProps,
 }: AppProps): JSX.Element {
   return (
-    <>
+    <Container>
       <Component {...pageProps} />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 100%;
+  height: 100vh;
+  max-height: 100%;
+`;
