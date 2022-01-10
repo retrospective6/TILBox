f0b791b63727946a641784ce0f25f48863bf06ed
hwookim <gusdn9754@gmail.com>
2022-01-11 00:23:10 +0900

    feat: TextInput의 상태 외부에서 관리하도록 로직 변경

--- client/src/components/common/TextInput.styles.tsx
+++ client/src/components/common/TextInput.styles.tsx
@@ -1,52 +1,63 @@
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
 import FONT from '@/styles/font';
+import { State } from '@/types';
 
-export const Container = styled.div`
+export const Container = styled.label`
+  width: min-content;
   white-space: nowrap;
 `;
 
-export const Label = styled.label`
+export const Label = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 4px;
+  margin-bottom: 2px;
 `;
 
 export const Title = styled.span`
   margin-right: 8px;
   ${FONT.caption1};
+  color: #000000;
 `;
 
-export const Feedback = styled.span`
-  ${FONT.caption4};
-  color: #c90909;
-`;
+interface MessageProps {
+  state?: State;
+}
 
-export const Hint = styled.span`
+export const Message = styled.span<MessageProps>`
   ${FONT.caption4};
-  color: #666666;
+  color: ${({ state }) => (state === 'error' ? '#c90909' : '#666666')};
 `;
 
 interface StyledInputProps {
+  state?: State;
   width?: string;
   height?: string;
-  isValid: boolean;
 }
+
+const stateCSS: { [keys in State]: SerializedStyles } = {
+  default: css`
+    border-color: #f3f3f3;
+    &::placeholder {
+      color: #cdcdcd;
+    }
+  `,
+  error: css`
+    border-color: #c90909;
+    &::placeholder {
+      color: #c90909;
+    }
+  `,
+};
 
 export const Input = styled.input<StyledInputProps>`
   width: ${(props) => props.width || '268px'};
   height: ${(props) => props.height || '28px'};
-  box-sizing: border-box;
+  padding: 7px 8px;
   border: 1px solid;
   border-radius: 8px;
-  padding: 7px 8px;
   background: #f3f3f3;
   ${FONT.body4};
 
-  ${({ isValid }) => css`
-    border-color: ${isValid ? '#f3f3f3' : '#c90909'};
-    &::placeholder {
-      color: ${isValid ? '#cdcdcd' : '#c90909'};
-    }
-  `};
+  ${({ state = 'default' }) => stateCSS[state]};
 `;
--- client/src/components/common/TextInput.tsx
+++ client/src/components/common/TextInput.tsx
@@ -1,47 +1,25 @@
-import React, { FocusEvent, InputHTMLAttributes, useState } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import * as Styled from './TextInput.styles';
-
-export type Rule = {
-  rule: (value: string) => boolean;
-  message: string;
-};
+import { State } from '@/types';
 
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   title?: string;
-  rules?: Rule[];
-  hint?: string;
+  state?: State;
+  message?: string;
   width?: string;
   height?: string;
 }
 
 export default function TextInput(props: TextInputProps): JSX.Element {
-  const { title, hint, rules } = props;
-  const [feedback, setFeedback] = useState<string>('');
-  const [isValid, setIsValid] = useState<boolean>(true);
-
-  const onBlur = (event: FocusEvent<HTMLInputElement>) => {
-    if (!rules) {
-      return;
-    }
-    const value = event.target.value;
-    const invalidRule = rules.find(({ rule }) => rule(value));
-    setIsValid(!invalidRule);
-    if (invalidRule) {
-      setFeedback(invalidRule.message);
-    }
-  };
+  const { title, state, message } = props;
 
   return (
     <Styled.Container>
       <Styled.Label>
-        <Styled.Title>{title}</Styled.Title>
-        {isValid ? (
-          <Styled.Hint>{hint}</Styled.Hint>
-        ) : (
-          <Styled.Feedback>{feedback}</Styled.Feedback>
-        )}
+        {title && <Styled.Title>{title}</Styled.Title>}
+        {message && <Styled.Message state={state}>{message}</Styled.Message>}
       </Styled.Label>
-      <Styled.Input onBlur={onBlur} isValid={isValid} {...props} />
+      <Styled.Input {...props} />
     </Styled.Container>
   );
 }
