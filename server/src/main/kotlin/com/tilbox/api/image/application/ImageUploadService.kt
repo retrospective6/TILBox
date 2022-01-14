09177e4e3f16973404a79e5b181f81a1f57cc3c7
K.S.KIM <nullable@kakao.com>
2022-01-14 22:44:33 +0900

    feat: 이미지 업로드 API 검증로직 추가 및 문서화

--- server/src/main/kotlin/com/tilbox/api/image/application/ImageUploadService.kt
+++ server/src/main/kotlin/com/tilbox/api/image/application/ImageUploadService.kt
@@ -8,18 +8,19 @@ import java.util.UUID
 @Transactional
 @Service
 class ImageUploadService(private val imageUploader: ImageUploader) {
-    fun upload(image: MultipartFile): ImageUploadResponse {
-        requireNotNull(image.originalFilename) { "올바르지 않은 이미지입니다." }
-        val fileName = generateFileName(image.originalFilename!!)
-        return image.inputStream.use { stream ->
+    fun upload(imageFile: MultipartFile): String {
+        requireNotNull(imageFile.originalFilename) { "올바르지 않은 이미지입니다." }
+        val fileName = generateFileName(imageFile.originalFilename!!)
+        val response = imageFile.inputStream.use { stream ->
             imageUploader.upload(
                 ImageUploadRequest(
                     stream,
-                    image.size,
+                    imageFile.size,
                     fileName
                 )
             )
         }
+        return response.url
     }
 
     private fun generateFileName(originalFileName: String): String {
