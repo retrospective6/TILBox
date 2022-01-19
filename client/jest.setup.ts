fd3477b2a635def6c952d822eec9dc941138ba73
hwookim <gusdn9754@gmail.com>
2022-01-20 01:17:18 +0900

    feat: jest 환경에서 msw를 이용하도록 설정

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,5 +1,12 @@
 /* eslint-disable */
 import '@testing-library/jest-dom/extend-expect';
+import { server } from '@mocks/apis/server';
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
 
 const mockFunc = () => {};
 Object.defineProperty(window, 'scrollTo', { value: mockFunc, writable: true });
