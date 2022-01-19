dae07bfdb6b6148b47938d8ec92e608654563615
hwookim <gusdn9754@gmail.com>
2022-01-20 02:10:16 +0900

    fix: mockApiURL 호출 오류 해결

--- client/__mocks__/apis/index.ts
+++ client/__mocks__/apis/index.ts
@@ -1,7 +1,3 @@
 import * as users from './users';
 
-export function mockApiURL(path: string): string {
-  return process.env.API_HOST + path;
-}
-
 export const mockApis = [...Object.values(users)];
--- client/__mocks__/apis/users.ts
+++ client/__mocks__/apis/users.ts
@@ -1,5 +1,5 @@
 import { rest } from 'msw';
-import { mockApiURL } from '@mocks/apis/index';
+import { mockApiURL } from '@mocks/apis/utils';
 
 export const mockedLogin = rest.post(mockApiURL('/login'), (req, res, ctx) =>
   res(ctx.json({ token: 'Bearer test-access-token' })),
