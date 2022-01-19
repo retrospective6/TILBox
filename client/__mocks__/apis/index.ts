2eeede57fec521cd3d09daabf68eb40e22c32684
hwookim <gusdn9754@gmail.com>
2022-01-20 01:34:58 +0900

    fix: mock api path 오류 해결

--- client/__mocks__/apis/index.ts
+++ client/__mocks__/apis/index.ts
@@ -1,3 +1,7 @@
 import * as users from './users';
 
+export function mockApiURL(path: string): string {
+  return process.env.API_HOST + path;
+}
+
 export const mockApis = [...Object.values(users)];
--- client/__mocks__/apis/users.ts
+++ client/__mocks__/apis/users.ts
@@ -1,5 +1,6 @@
 import { rest } from 'msw';
+import { mockApiURL } from '@mocks/apis/index';
 
-export const mockedLogin = rest.post('/', (req, res, ctx) =>
+export const mockedLogin = rest.post(mockApiURL('/login'), (req, res, ctx) =>
   res(ctx.json({ token: 'Bearer test-access-token' })),
 );
