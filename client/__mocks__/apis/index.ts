aa51e8ae706ccd0ec9d70d98cc6e99e042778017
hwookim <gusdn9754@gmail.com>
2022-01-23 04:20:08 +0900

    feat: post api 연동

--- client/__mocks__/apis/index.ts
+++ client/__mocks__/apis/index.ts
@@ -1,7 +1,9 @@
 import * as users from './users';
 import * as images from './images';
+import * as posts from './posts';
 
 export const mockApis = Object.values({
   users,
   images,
+  posts,
 }).flatMap((apis: object) => Object.values(apis));
