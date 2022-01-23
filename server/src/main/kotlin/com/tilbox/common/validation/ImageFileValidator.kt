51069a7a3a32a28acec5515a712b30ee9e866d99
K.S.KIM <nullable@kakao.com>
2022-01-23 19:51:01 +0900

    refactor: 이미지 파일 검증시 content type이 없는 경우 처리방식 개선

--- server/src/main/kotlin/com/tilbox/common/validation/ImageFileValidator.kt
+++ server/src/main/kotlin/com/tilbox/common/validation/ImageFileValidator.kt
@@ -1,7 +1,6 @@
 package com.tilbox.common.validation
 
 import org.springframework.web.multipart.MultipartFile
-import java.util.Objects
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
@@ -9,15 +8,11 @@ val ALLOWED_CONTENT_TYPES = listOf("image/gif", "image/png", "image/jpg", "image
 
 class ImageFileValidator : ConstraintValidator<ImageFile?, MultipartFile> {
     override fun isValid(file: MultipartFile, context: ConstraintValidatorContext): Boolean {
-        val contentType = getContentType(file)
+        val contentType = file.contentType ?: return false
         return isSupportedContentType(contentType)
     }
 
-    private fun getContentType(file: MultipartFile): String? {
-        return Objects.requireNonNull(file.contentType, "파일 형식을 찾을 수 없습니다.")
-    }
-
-    private fun isSupportedContentType(contentType: String?): Boolean {
+    private fun isSupportedContentType(contentType: String): Boolean {
         return ALLOWED_CONTENT_TYPES.any { it == contentType }
     }
 }
