7499ee920ace8cd403248cbf6e1b04ade8614df7
ksy90101 <ksy90101@gmail.com>
2022-01-23 19:24:41 +0900

    feat: axios header에 content-type 추가

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -3,4 +3,7 @@ import axios from 'axios';
 export const http = axios.create({
   baseURL: 'http://localhost:8080/v1/',
   timeout: 30000,
+  headers: {
+    'content-type': 'application/json',
+  },
 });
