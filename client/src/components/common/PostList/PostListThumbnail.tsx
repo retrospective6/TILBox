b4366c9c08fcfb7c9e384e7cfe725698df4b302c
hwookim <gusdn9754@gmail.com>
2022-01-23 20:01:19 +0900

    fix: Post.Thumbnail 변경 누락분 수정

--- client/src/components/common/PostList/PostListThumbnail.tsx
+++ client/src/components/common/PostList/PostListThumbnail.tsx
@@ -9,7 +9,7 @@ export default function PostListThumbnail(
 ): JSX.Element {
   const { title, thumbnail } = props;
   const [imgSrc, setImgSrc] = useState<string>();
-  const [gradient, setGradient] = useState<ThumbnailGradation>();
+  const [gradation, setGradation] = useState<ThumbnailGradation>();
 
   useEffect(() => {
     if (thumbnail.type === 'image') {
@@ -17,11 +17,11 @@ export default function PostListThumbnail(
       return;
     }
 
-    setGradient(thumbnail.value as ThumbnailGradation);
+    setGradation(thumbnail.value as ThumbnailGradation);
   }, [thumbnail]);
 
   return (
-    <Styled.Thumbnail start={gradient?.start} end={gradient?.end}>
+    <Styled.Thumbnail start={gradation?.start} end={gradation?.end}>
       {imgSrc && <Styled.ThumbnailImg src={imgSrc} alt={title} layout="fill" />}
       <Styled.ThumbnailText length={title.length}>{title}</Styled.ThumbnailText>
     </Styled.Thumbnail>
