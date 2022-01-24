cefa6356d5076e4d480805b997982dc7777880e6
hwookim <gusdn9754@gmail.com>
2022-01-24 23:55:13 +0900

    feat: user.getProfile api 임시 구현

--- client/__mocks__/apis/users.ts
+++ client/__mocks__/apis/users.ts
@@ -1,6 +1,12 @@
 import { rest } from 'msw';
 import { mockApiURL } from '@mocks/apis/utils';
+import { PROFILE } from '@mocks/data/users';
 
 export const mockedLogin = rest.post(mockApiURL('/login'), (req, res, ctx) =>
   res(ctx.json({ accessToken: 'Bearer test-access-token' })),
+);
+
+export const mockedGetProfile = rest.get(
+  mockApiURL('/users/profile'),
+  (req, res, ctx) => res(ctx.json({ ...PROFILE })),
 );
--- client/src/apis/users.ts
+++ client/src/apis/users.ts
@@ -1,4 +1,6 @@
 import { tilAxios } from '@/apis/utils';
+import { Profile } from '@/types/User';
+import { getAuthHeader } from '@/apis/utils/auth';
 
 export interface LoginRequest {
   email: string;
@@ -11,4 +13,10 @@ export interface LoginResponse {
 
 export function login(param: LoginRequest): Promise<LoginResponse> {
   return tilAxios.post('/login', param).then((res) => res.data);
+}
+
+export function getProfile(): Promise<Profile> {
+  return tilAxios
+    .get('/users/profile', getAuthHeader())
+    .then((res) => res.data);
 }
