f0f919128cc0d465b51de067672b3d567d246680
hwookim <gusdn9754@gmail.com>
2022-01-26 22:03:48 +0900

    feat: swr이 각 스토리북마다 별개의 캐시를 가지도록 설정
    - api 요청 실패 시 재요청 방지
    - 같은 키의 요청 매번 다시 요청하도록 변경

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -3,9 +3,23 @@ import { initialize, mswDecorator } from 'msw-storybook-addon';
 import { mockApis } from '../__mocks__/apis';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import * as NextImage from 'next/image';
+import { SWRConfig } from 'swr';
 
 initialize();
-export const decorators = [mswDecorator];
+export const decorators = [
+  mswDecorator,
+  (Story) => (
+    <SWRConfig
+      value={{
+        dedupingInterval: 0,
+        errorRetryCount: 0,
+        provider: () => new Map(),
+      }}
+    >
+      <Story />
+    </SWRConfig>
+  ),
+];
 
 export const parameters = {
   msw: mockApis,
