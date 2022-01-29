30a5ba34c5385140985709583c1e59e7f3c39a7d
hwookim <gusdn9754@gmail.com>
2022-01-29 21:23:06 +0900

    fix: return type 명시

--- client/__mocks__/cookie.ts
+++ client/__mocks__/cookie.ts
@@ -3,22 +3,25 @@ import { StoryContext } from '@storybook/react';
 let cookies: { [keys in string]: string } = {};
 
 const cookie = {
-  set(key: string, value: string) {
+  set(key: string, value: string): void {
     cookies[key] = value;
   },
 
-  get(key: string) {
+  get(key: string): string | undefined {
     return cookies[key];
   },
 
-  remove(key: string) {
+  remove(key: string): void {
     this.set(key, '');
   },
 };
 
 export default cookie;
 
-export function cookieDecorator(story: any, { parameters }: StoryContext) {
+export function cookieDecorator(
+  story: () => unknown,
+  { parameters }: StoryContext,
+): unknown {
   cookies = {};
   if (parameters && parameters.cookie) {
     cookies = parameters.cookie;
