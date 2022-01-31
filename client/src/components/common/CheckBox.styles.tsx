8da44112be157f33426ee5ed7f5a307be616a1b0
hwookim <gusdn9754@gmail.com>
2022-01-31 23:13:00 +0900

    feat: CheckBox 구현

---
+++ client/src/components/common/CheckBox.styles.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const CheckBox = styled.input`
+  display: none;
+`;
+
+export const Label = styled.label`
+  margin-left: 10px;
+  cursor: pointer;
+`;
---
+++ client/src/components/common/CheckBox.tsx
@@ -0,0 +1,35 @@
+import React, {
+  InputHTMLAttributes,
+  MouseEvent,
+  useRef,
+  useState,
+} from 'react';
+import * as Styled from './CheckBox.styles';
+
+import CheckBoxIcon from '@/assets/icon/CheckBox.svg';
+
+export interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+export default function CheckBox(props: CheckBoxProps): JSX.Element {
+  const { label, checked } = props;
+  const [check, setCheck] = useState<boolean>(checked || false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setCheck(!check);
+    inputRef.current?.click();
+  };
+
+  return (
+    <Styled.Container onClick={handleClick}>
+      <CheckBoxIcon fill={check ? '#0068D4' : '#666666'} />
+      <Styled.Label>
+        <Styled.CheckBox {...props} type="checkbox" ref={inputRef} />
+        {label}
+      </Styled.Label>
+    </Styled.Container>
+  );
+}
