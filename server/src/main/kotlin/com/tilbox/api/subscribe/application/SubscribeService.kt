3dfe5fdb9941ddce9459cda0594a81ad918c80e7
kimjunseok <jnunseok@gmail.com>
2022-02-06 20:23:33 +0900

    refactor: SubscribeService 검증 로직 추가

--- server/src/main/kotlin/com/tilbox/api/subscribe/application/SubscribeService.kt
+++ server/src/main/kotlin/com/tilbox/api/subscribe/application/SubscribeService.kt
@@ -2,18 +2,33 @@ package com.tilbox.api.subscribe.application
 
 import com.tilbox.core.subscribe.domain.entity.Subscribe
 import com.tilbox.core.subscribe.domain.repository.SubscribeRepository
+import com.tilbox.core.user.domain.UserRepository
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Transactional
 @Service
-class SubscribeService(private val subscribeRepository: SubscribeRepository) {
+class SubscribeService(
+    private val subscribeRepository: SubscribeRepository,
+    private val userRepository: UserRepository
+) {
     fun subscribe(userId: Long, followerId: Long) {
-        val subscribe = Subscribe(userId, followerId)
-        subscribeRepository.save(subscribe)
+        check(userRepository.existsById(followerId)) {"사용자가 존재하지 않습니다. followerId=$followerId"}
+        check(
+            !subscribeRepository.existsByUserIdAndFollowerId(
+                userId,
+                followerId
+            )
+        ) {"이미 구독한 사용자입니다. userId=$userId, followId=$followerId"}
+        subscribeRepository.save(Subscribe(userId, followerId))
     }
 
     fun unsubscribe(userId: Long, followerId: Long) {
-        subscribeRepository.deleteByUserIdAndFollowerId(userId, followerId)
+        check(userRepository.existsById(followerId)) {"사용자가 존재하지 않습니다. followerId=$followerId"}
+        val subscribe = subscribeRepository.findByUserIdAndFollowerId(userId, followerId)
+            .orElseThrow {
+                IllegalStateException("구독중이 아닙니다. userId=$userId, followerId=$followerId")
+            }
+        subscribeRepository.delete(subscribe)
     }
 }
--- server/src/main/kotlin/com/tilbox/core/subscribe/domain/repository/SubscribeRepository.kt
+++ server/src/main/kotlin/com/tilbox/core/subscribe/domain/repository/SubscribeRepository.kt
@@ -2,7 +2,10 @@ package com.tilbox.core.subscribe.domain.repository
 
 import com.tilbox.core.subscribe.domain.entity.Subscribe
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface SubscribeRepository : JpaRepository<Subscribe, Long> {
-    fun deleteByUserIdAndFollowerId(userId: Long, followerId: Long)
+    fun existsByUserIdAndFollowerId(userId: Long, followerId: Long): Boolean
+
+    fun findByUserIdAndFollowerId(userId: Long, followerId: Long): Optional<Subscribe>
 }
