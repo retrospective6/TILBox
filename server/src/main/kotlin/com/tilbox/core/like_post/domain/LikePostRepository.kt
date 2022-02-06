ad891fe569fcafbb95e88777576fb95f7d5f82f0
K.S.KIM <nullable@kakao.com>
2022-02-06 23:26:49 +0900

    refactor: 불필요한 repository 어노테이션 제거

--- server/src/main/kotlin/com/tilbox/core/like_post/domain/LikePostRepository.kt
+++ server/src/main/kotlin/com/tilbox/core/like_post/domain/LikePostRepository.kt
@@ -1,10 +1,8 @@
 package com.tilbox.core.like_post.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import java.util.Optional
 
-@Repository
 interface LikePostRepository : JpaRepository<LikePost, Long> {
     fun findByPostIdAndUserId(postId: Long, userId: Long): Optional<LikePost>
 
