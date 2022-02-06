3718320e4027db601e287f56331a221d04125878
kimjunseok <jnunseok@gmail.com>
2022-02-06 22:26:05 +0900

    refactor: 구독 서비스 테스트 코드 추가

--- server/src/test/kotlin/com/tilbox/api/subscribe/application/SubscribeServiceTest.kt
+++ server/src/test/kotlin/com/tilbox/api/subscribe/application/SubscribeServiceTest.kt
@@ -1,11 +1,20 @@
 package com.tilbox.api.subscribe.application
 
 import com.tilbox.core.subscribe.domain.repository.SubscribeRepository
+import com.tilbox.core.user.domain.Password
+import com.tilbox.core.user.domain.Profile
+import com.tilbox.core.user.domain.RegistrationType
+import com.tilbox.core.user.domain.User
+import com.tilbox.core.user.domain.UserRepository
+import com.tilbox.core.user.domain.UserRole
+import com.tilbox.core.user.domain.UserStatus
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
+import java.time.LocalDateTime
 import javax.transaction.Transactional
 
 @Transactional
@@ -14,26 +23,67 @@ import javax.transaction.Transactional
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class SubscribeServiceTest(
     private val subscribeService: SubscribeService,
-    private val subscribeRepository: SubscribeRepository
+    private val subscribeRepository: SubscribeRepository,
+    private val userRepository: UserRepository
 ) {
+    private lateinit  var user: User
+    private lateinit  var follower: User
+
+    @BeforeEach
+    fun setUp() {
+        user = userRepository.save(
+            User(
+                "nullable",
+                "nullable@kakao.com",
+                Profile(
+                    "KS-KIM",
+                    null,
+                    "I love kotlin. I hate javascript. javascript is not a language."
+                ),
+                Password("password"),
+                UserStatus.AUTHENTICATED,
+                RegistrationType.EMAIL,
+                UserRole.USER,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                null
+            )
+        )
+        follower = userRepository.save(
+            User(
+                "not_nullable",
+                "nullable@kakao.com",
+                Profile(
+                    "Tigger",
+                    null,
+                    "I love kotlin. I hate php. php is not a language."
+                ),
+                Password("password"),
+                UserStatus.AUTHENTICATED,
+                RegistrationType.EMAIL,
+                UserRole.USER,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                null
+            )
+        )
+    }
 
     @Test
     fun `구독하기`() {
-        subscribeService.subscribe(0L, 1L)
-        subscribeService.subscribe(0L, 2L)
+        subscribeService.subscribe(user.id, follower.id)
 
         val actual = subscribeRepository.findAll()
-        actual.size shouldBe 2
+        actual.size shouldBe 1
     }
 
     @Test
     fun `구독취소하기`() {
-        subscribeService.subscribe(0L, 1L)
-        subscribeService.subscribe(0L, 2L)
+        subscribeService.subscribe(user.id, follower.id)
 
-        subscribeService.unsubscribe(1L, 2L)
+        subscribeService.unsubscribe(user.id, follower.id)
 
         val actual = subscribeRepository.findAll()
-        actual.size shouldBe 1
+        actual.size shouldBe 0
     }
 }
