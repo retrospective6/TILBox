a85acad5de15f3f16fabb3dd08186984083edcf0
hwookim <gusdn9754@gmail.com>
2022-02-10 00:11:22 +0900

    fix: test render 시 QueryClientProvider 포함

--- client/__tests__/testUtils/renderWithProvider.tsx
+++ client/__tests__/testUtils/renderWithProvider.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { render, RenderResult, RenderOptions } from '@testing-library/react';
 import { SWRConfig } from 'swr';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function renderWithProvider(
   component: JSX.Element,
   options?: Omit<RenderOptions, 'queries'>,
 ): RenderResult {
   return render(
-    <SWRConfig
-      value={{
-        dedupingInterval: 0,
-        errorRetryCount: 0,
-        provider: () => new Map(),
-      }}
-    >
-      {component}
-    </SWRConfig>,
+    <QueryClientProvider client={new QueryClient()}>
+      <SWRConfig
+        value={{
+          dedupingInterval: 0,
+          errorRetryCount: 0,
+          provider: () => new Map(),
+        }}
+      >
+        {component}
+      </SWRConfig>
+    </QueryClientProvider>,
     options,
   );
 }
