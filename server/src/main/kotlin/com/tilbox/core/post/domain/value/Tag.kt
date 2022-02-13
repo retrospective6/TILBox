5bfcf77f562123d4b5240d8a24603815c849fb2d
K.S.KIM <nullable@kakao.com>
2022-02-13 19:09:31 +0900

    fix: 테스트 실패 수정

--- server/src/main/kotlin/com/tilbox/core/post/domain/value/Tag.kt
+++ server/src/main/kotlin/com/tilbox/core/post/domain/value/Tag.kt
@@ -7,4 +7,20 @@ import javax.persistence.Embeddable
 class Tag(
     @Column(nullable = false, length = 10)
     val name: String
-)
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Tag
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
