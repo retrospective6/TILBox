02be9c9aea2169857e3f4c1c1fe98aa045985fe8
K.S.KIM <nullable@kakao.com>
2022-02-13 20:35:05 +0900

    refactor: 게시물 비교 테스트에서 모든 필드를 비교하도록 수정

--- server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
+++ server/src/main/kotlin/com/tilbox/core/post/domain/value/Tags.kt
@@ -33,4 +33,19 @@ class Tags(
             return Tags(tags)
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Tags
+
+        if (items != other.items) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return items.hashCode()
+    }
 }
