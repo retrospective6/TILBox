9085c31a54079e46403ade0e6037bc3439a7dc4a
hwookim <gusdn9754@gmail.com>
2022-02-26 23:12:09 +0900

    test: classifyPosts 테스트 추가

--- client/__tests__/utils/index.test.ts
+++ client/__tests__/utils/index.test.ts
@@ -1,4 +1,6 @@
-import { range } from '@/utils';
+import { classifyPosts, range } from '@/utils';
+import Post from '@/types/Post';
+import { POST } from '@mocks/data/posts';
 
 describe('range', () => {
   test.each([
@@ -9,5 +11,67 @@ describe('range', () => {
     const result = range(size, start);
     expect(result).toHaveLength(size);
     expect(result[0]).toEqual(start);
+  });
+});
+
+describe('classifyPosts', () => {
+  test(`
+    {
+      year: number;
+      month: number;
+      posts: Post[];
+    } 형태로 분류
+  `, () => {
+    const posts: Post[] = [
+      { ...POST, createdAt: new Date('2022.02.27') },
+      { ...POST, createdAt: new Date('2022.02.26') },
+    ];
+
+    const result = classifyPosts(posts);
+
+    expect(result[0]).toHaveProperty('year', 2022);
+    expect(result[0]).toHaveProperty('month', 2);
+    expect(result[0]).toHaveProperty('posts', posts);
+  });
+
+  test('최근 년도가 앞에 오도록 정렬', () => {
+    const posts: Post[] = [
+      { ...POST, createdAt: new Date('2020.02.27') },
+      { ...POST, createdAt: new Date('2022.02.27') },
+      { ...POST, createdAt: new Date('2021.02.27') },
+    ];
+
+    const result = classifyPosts(posts);
+
+    expect(result[0]).toHaveProperty('year', 2022);
+    expect(result[0]).toHaveProperty('month', 2);
+  });
+
+  test('최근 월이 앞에 오도록 정렬', () => {
+    const posts: Post[] = [
+      { ...POST, createdAt: new Date('2022.01.27') },
+      { ...POST, createdAt: new Date('2022.05.27') },
+      { ...POST, createdAt: new Date('2022.04.27') },
+    ];
+
+    const result = classifyPosts(posts);
+
+    expect(result[0]).toHaveProperty('year', 2022);
+    expect(result[0]).toHaveProperty('month', 5);
+  });
+
+  test('최근 일자가 앞에 오도록 정렬', () => {
+    const posts: Post[] = [
+      { ...POST, createdAt: new Date('2022.02.26') },
+      { ...POST, createdAt: new Date('2022.02.28') },
+      { ...POST, createdAt: new Date('2022.02.27') },
+    ];
+
+    const result = classifyPosts(posts);
+
+    expect(result[0].posts[0]).toHaveProperty(
+      'createdAt',
+      new Date('2022.02.28'),
+    );
   });
 });
